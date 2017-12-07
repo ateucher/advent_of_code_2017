b054ff07b8c870554fb5a224eb50c73c60ce4c74
Andy Teucher <andy.teucher@gmail.com>
2017-12-07 10:02:10 -0800

    Solve part 1

--- day07.R
+++ day07.R
@@ -1,18 +1,20 @@
 library(stringr)
 library(purrr)
 library(dplyr)
+library(tidyr)
 
-lines <- readLines("day07_test_input.txt")
+lines <- readLines("day07_input.txt")
 
 ## tibble-driven
 tbl <- str_split(lines, "\\s\\(|\\)|\\s->\\s", simplify = TRUE)[,-3] %>% 
   as_tibble() %>% 
   set_names(c("name", "wt", "children")) %>% 
-  mutate(children = map(children, ~ ifelse(.x == "", 
-                                           NA_character_, 
-                                           str_split(.x, ", "))), 
-         is_leaf = ifelse(is.na(children), TRUE, FALSE))
-  
+  mutate(children = map(children, ~ str_split(.x, ", ")[[1]])) %>% 
+  unnest()
+
+# The base program should be the only one that is a parent but not a child
+setdiff(unique(tbl$name), unique(tbl$children))
+
 
 
 
