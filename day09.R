b66b79c5d419bfdc3e84258bd041417ed2270d6e
ateucher <andy.teucher@gmail.com>
2017-12-09 23:41:50 -0800

    Day 2 - tests work, but answer is incorrect

--- day09.R
+++ day09.R
@@ -3,7 +3,7 @@ library(stringr)
 
 garbage_score <- function(x) {
   
-  x <- sanitize(x)
+  x <- sanitize(x)[[1]]
   
   split_up <- x %>%
     str_split("") %>% 
@@ -26,9 +26,12 @@ garbage_score <- function(x) {
   count
 }
 
-sanitize <- function(x) {
+sanitize <- function(x, removed) {
+  n_orig <- nchar(x)
+  n_removed <- 0
   while (str_detect(x, "!")) {
     x <- str_replace(x, "!.", "")
+    n_removed <- n_removed + 2
   }
   
   x <- str_replace_all(x, ",", "")
@@ -37,7 +40,9 @@ sanitize <- function(x) {
   # https://stackoverflow.com/a/35271017
   pattern <- "\\<(?>[^>]*(?R)?)*\\>"
   
-  gsub(pattern, "", x, perl = TRUE)
+  sanitized <- gsub(pattern, "<>", x, perl = TRUE)
+  list(str_replace_all(sanitized, "<>", ""), 
+       n_orig - (nchar(sanitized) + n_removed))
 }
 
 grp_1 <- "{}" # 1
@@ -62,3 +67,23 @@ input <- readLines("day09_input.txt")
 
 garbage_score(input)
 # 20530
+
+# Part 2:
+
+test_1 <- '{<>' # 0 characters.
+test_2 <- '{<random characters>}' # 17 characters.
+test_3 <- '{<<<<>}' # 3 characters.
+test_4 <- '{<{!>}>}' # 2 characters.
+test_5 <- '{<!!>}' # 0 characters.
+test_6 <- '{<!!!>>}' # 0 characters.
+test_7 <- '{<{o"i!a,<{i<a>}' # 10 characters.
+
+expect_equal(sanitize(test_1)[[2]], 0)
+expect_equal(sanitize(test_2)[[2]], 17)
+expect_equal(sanitize(test_3)[[2]], 3)
+expect_equal(sanitize(test_4)[[2]], 2)
+expect_equal(sanitize(test_5)[[2]], 0)
+expect_equal(sanitize(test_6)[[2]], 0)
+expect_equal(sanitize(test_7)[[2]], 10)
+
+sanitize(input)[[2]] # 11453 is too high
